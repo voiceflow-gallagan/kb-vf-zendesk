e39e579eacdf6911fcb53dd356aa413cfc51e157
NiKo <53017296+voiceflow-gallagan@users.noreply.github.com>
2023-12-07 01:47:14 +0100

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,10 +3,6 @@ module.exports = {
     {
       name: 'vf-kb-zendesk-import',
       script: 'app.js',
-      env_production: {
-        PORT: 3002,
-        NODE_ENV: 'production',
-      },
       watch: false,
     },
   ],
